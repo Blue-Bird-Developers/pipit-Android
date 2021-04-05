c1397a528e4f4f65b8696f1ccf23b58d2a22ba7a
arimyeon <dusdkfla@naver.com>
2021-04-06 00:59:01 +0900

    SplashActivity 네트워크 연결 확인 로직 추가

--- app/src/main/java/com/bluebird/pipit/ui/SplashActivity.kt
+++ app/src/main/java/com/bluebird/pipit/ui/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.bluebird.pipit.ui
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bluebird.pipit.ui.login.LoginMainActivity
@@ -9,6 +11,21 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        while (true) {
+            if (checkInternetConnection()){
+                startLoginMain()
+                break
+            }
+        }
+    }
+
+    private fun checkInternetConnection() : Boolean{
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities = cm.getNetworkCapabilities(cm.activeNetwork)
+        return capabilities != null
+    }
+
+    private fun startLoginMain(){
         val intent = Intent(this, LoginMainActivity::class.java)
         startActivity(intent)
         finish()
