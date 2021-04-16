62a505657780cbcdc233746a59596078e20c6386
arimyeon <sdr04161@gmail.com>
2021-04-16 14:54:31 +0900

    PortalCertifyActivity 생성

---
+++ app/src/main/java/com/bluebird/pipit/ui/login/PortalCertifyActivity.kt
@@ -0,0 +1,12 @@
+package com.bluebird.pipit.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bluebird.pipit.R
+
+class PortalCertifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_portal_certify)
+    }
+}
