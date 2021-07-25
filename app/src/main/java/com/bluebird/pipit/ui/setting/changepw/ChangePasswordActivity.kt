613e8207217709b2404e0901694a4fd1c88075d9
arimyeon <dusdkfla@naver.com>
2021-07-25 22:59:54 +0900

    fonts.xml 추가 및 전체적인 글씨체 수정

---
+++ app/src/main/java/com/bluebird/pipit/ui/setting/changepw/ChangePasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.bluebird.pipit.ui.setting.changepw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bluebird.pipit.R
+
+class ChangePasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_password)
+    }
+}
