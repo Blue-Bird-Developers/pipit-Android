ab294c22e37387b44e50cfcbe33b4c955e672dfb
arimyeon <dusdkfla@naver.com>
2021-08-09 17:02:09 +0900

    CheckPasswordActivity, ChangePasswordActivity 구현

--- app/src/main/java/com/bluebird/pipit/ui/login/ResetPasswordResultActivity.kt
+++ app/src/main/java/com/bluebird/pipit/ui/login/ResetPasswordResultActivity.kt
@@ -27,7 +27,7 @@ class ResetPasswordResultActivity : AppCompatActivity() {
 
     private fun directLoginActivity(){
         val intent = Intent(this, LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
     }
 }
--- app/src/main/java/com/bluebird/pipit/ui/setting/changepw/CheckPasswordActivity.kt
+++ app/src/main/java/com/bluebird/pipit/ui/setting/changepw/CheckPasswordActivity.kt
@@ -1,8 +1,10 @@
 package com.bluebird.pipit.ui.setting.changepw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bluebird.pipit.R
+import com.bluebird.pipit.ui.login.AccountCertifyActivity
 import kotlinx.android.synthetic.main.activity_check_password.*
 
 class CheckPasswordActivity : AppCompatActivity() {
@@ -19,5 +21,15 @@ class CheckPasswordActivity : AppCompatActivity() {
 
     private fun setListeners(){
         backBtn.setOnClickListener { finish() }
+        checkPasswordBtn.setOnClickListener {
+            val intent = Intent(this, ChangePasswordActivity::class.java)
+            startActivity(intent)
+        }
+        resetPasswordBtn.setOnClickListener { directAccountCertifyActivity() }
+    }
+
+    private fun directAccountCertifyActivity(){
+        val intent = Intent(this, AccountCertifyActivity::class.java)
+        startActivity(intent)
     }
 }
